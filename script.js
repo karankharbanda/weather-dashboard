4a33dc035706b9d9bbc3f6f58a446bf8f3cabf45
karankharbanda <karankharbanda2098@gmail.com>
2025-06-02 11:00:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,20 @@ async function getWeather(type){
         errorDiv.textContent = "Please enter a city name";
         return;
         }
-        url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-        fetchAndShow(url);
+
+        const geoUrl = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`);
+        const geoData = await geoUrl.json();
+        const cityName = geoData[0].name;
+
+        if(!geoData.length){
+            errorDiv.textContent=`No data for ${city} city found`;
+            return;
+        }
+        const lon = geoData[0].lon;
+        const lat = geoData[0].lat;
+
+        url=`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,daily,alerts&units=metric&appid=${apiKey}`;
+        fetchAndShow(url,cityName,lat,lon);
     }
 
     else if(type === "location"){
@@ -22,11 +34,17 @@ async function getWeather(type){
              errorDiv.textContent = "Location not found";
             return;
         }
-        navigator.geolocation.getCurrentPosition((position) => {
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-            url=`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-            fetchAndShow(url);
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+
+            const revUrl = await fetch(`https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${apiKey}`);
+            const revdata = await revUrl.json();
+
+            const cityName = revdata[0]?.name || 'Current Location';
+            
+            url=`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,daily,alerts&units=metric&appid=${apiKey}`;
+            fetchAndShow(url,cityName,lat,lon);
         },
     () => {
          errorDiv.textContent = "Not able to find your location. ";
@@ -34,7 +52,7 @@ async function getWeather(type){
     }
 
 
-    async function fetchAndShow(url) {
+    async function fetchAndShow(url,cityName,lat,lon) {
         
     
     try{
@@ -44,43 +62,39 @@ async function getWeather(type){
         }
         const data = await response.json();
 
-        if(data.cod !== 200){
-              errorDiv.textContent = data.message;
-        }
 
-        if(!data.weather || !data.weather[0]){
+        if(!data.current  || !data.current.weather || !data.current.weather[0]){
             throw new Error("Unexpected error occured")
         }
 
-        const icon = data.weather[0].icon;
+        const icon = data.current.weather[0].icon;
         const iconurl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
         weatherDiv.innerHTML=`
-        <div class="bg-blue-300 p-6 rounded-lg shadow-lg w-full max-w-md mx-auto space-y-6"> 
+        <div class="bg-blue-300 p-3 rounded-lg shadow-lg w-full max-w-md mx-auto space-y-6"> 
         <div class="text-center">
-        <h2 class="text-xl font-semibold mb-4">${data.name},${data.sys.country}</h2>
-        <p class = "mb-4 text-gray-600">Weather: ${data.weather[0].main} (${data.weather[0].description})</p>
-        <img class="mx-auto mb-2" src = "${iconurl}" alt = "Weather Icon" />
+        <h2 class="text-xl font-semibold">${cityName}</h2>
+        <p class = "mb-4 text-gray-600">Weather: ${data.current.weather[0].main} (${data.current.weather[0].description})</p>
+        <img class="mx-auto" src = "${iconurl}" alt = "Weather Icon" />
         </div>
-        <hr class="border=gray-300">
+        <hr class="border-gray-300">
         <div>
         <h3 class="text-lg font-semibold mb-2">Temperature</h3>
-        <p class = "mb-1">Current: ${data.main.temp}&deg;C</p>
-        <p class = "mb-1">Feels like: ${data.main.feels_like}&deg;C</p>
-        <p class = "mb-1">Max: ${data.main.temp_min}&deg;C</p>
-        <p class = "mb-1">Min: ${data.main.temp_max}&deg;C</p>
+        <p class = "">Current Temp: ${data.current.temp}&deg;C</p>
+        <p class = "mb-1">Feels like: ${data.current.feels_like}&deg;C</p>
+        
         </div>
         <hr class="border-gray-300">
         <div>
         <h3 class="text-lg font-semibold mb-1">Atmosphere</h3>
-        <p class = "mb-1">Humidity: ${data.main.humidity}%</p>
-        <p class = "mb-1">Pressure: ${data.main.pressure} hPa</p>
-        <p class = "mb-1">Visibility: ${data.visibility / 1000} km</p>
+        <p class = "mb-1">Humidity: ${data.current.humidity}%</p>
+        <p class = "mb-1">Pressure: ${data.current.pressure} hPa</p>
+        <p class = "mb-1">Visibility: ${data.current.visibility / 1000} km</p>
         </div>
         <hr class="border-gray-300">
         <h3 class="text-lg font-semibold mb-1">Wind And Location</h3>
         <div>
-        <p class = "mb-1">Wind: ${data.wind.speed}m/s</p>
-        <p class = "mb-1">Coordinates: ${data.coord.lat},${data.coord.lon}</p>
+        <p class = "mb-1">Wind Speed: ${data.current.wind_speed}m/s</p>
+        <p class = "mb-1">Coordinates: ${lat},${lon}</p>
         </div>
         </div>
         `;
